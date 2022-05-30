92f78f8f95f9b69de433bca6f5abe0eb49a39c42
emaqueda <e.maq.cha@gmail.com>
2022-05-30 06:43:20 -0500

    proyectocorregido

--- src/componentes/CartContext.js
+++ src/componentes/CartContext.js
@@ -2,29 +2,38 @@ import React, { createContext, useState } from "react";
 
 export const CartContext = createContext(); 
 
-const inCart = []; 
 
 export const CartProvider = ({children}) => {
 
-    const addItem = (id, title, precio, qty) => {
-       const newProduct = {
-           title: title,
-           precio: precio,
-           qty: qty,
-           id: id
+    const [items, setItems] = useState([])
+    
+    const addItem = (data) => {
+       let found = undefined
 
+       const newProduct = {
+           id: data.id,
+           title: data.title,
+           precio: data.precio,
+           qty: Number(data.qty)
        }
 
-    const found = items.find(item=> item.id == id); 
-    
-    if(!found){
-        inCart.push(newProduct)
-        setItems(inCart)
+       items.forEach((item, index)=>{
+           if(item.id == data.id){
+               found = index
+           }
+       })
 
-        console.log(items)
-    }else{
-        console.log("El producto ya se agrego")
-    }
+       if(found == undefined) {
+           items.push(newProduct)
+           setItems(items)
+           console.log(items)
+           console.log("Producto agregado")
+       } else {
+           items[found].qty += Number(data.qty)
+           setItems(items)
+           console.log("Producto modificado")
+           console.log(items)
+       }
     }
 
     const removeItem = (itemId) => {
@@ -37,8 +46,8 @@ export const CartProvider = ({children}) => {
         }); 
 
         if (found){
-            const arrayNuevo = inCart.splice(found, 1)
-            setItems(arrayNuevo)
+            items.splice(found, 1)
+            setItems(items)
         }else {
             console.log("No se encontro el producto")
         }
@@ -58,7 +67,7 @@ export const CartProvider = ({children}) => {
         }
     }
 
-    const [items, setItems] = useState(inCart)
+    
     
     return (
         <CartContext.Provider value={[addItem, removeItem, clear, isInCart]}>
